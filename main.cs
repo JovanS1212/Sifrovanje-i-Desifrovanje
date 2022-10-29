a41eb3b2378287b8a36ae8ff203bd25d9ad793c1
Јован Стефановић <107138433+JovanS1212@users.noreply.github.com>
2022-10-29 21:46:46 +0000

    Dodat prototip prvog nacina sifrovanja

--- main.cs
+++ main.cs
@@ -1,57 +1,102 @@
 using System;
 using System.Text;
 using System.Threading;
+using System.IO;
 
 class Program
 {
-        struct Operacija
+    struct Operacija
+    {
+        public string datoteka;
+        public int brSifre;
+        public bool sifrovanje; //glup naziv
+        public string kljuc;
+    }
+    static bool sifra1(Operacija operacija)
+    {
+        StreamWriter sw;
+        for (int i = 0; i < operacija.kljuc.Length; i++)
         {
-            public string datoteka;
-            public int brSifre;
-            public bool sifrovanje; //glup naziv
-            public string kljuc;
+            if (!char.IsLetterOrDigit(operacija.kljuc[i]))
+            {
+                return false;
+            }
         }
-        static void IspisRadaPrograma(string greska)
+        StreamReader sb = new StreamReader(operacija.datoteka);
+        if (operacija.sifrovanje)
         {
-            Console.WriteLine("program radi tako sto:");
-            Console.WriteLine("bla bla bla");
-            Console.WriteLine(greska);
+            sw = new StreamWriter("sifrovana_" + operacija.datoteka);
+            while (sb.EndOfStream)
+            {
+                char[] temp = sb.ReadLine().ToCharArray();
+
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    temp[i] += operacija.kljuc[i % operacija.kljuc.Length];
+                }
+                sw.WriteLine(temp);
+            }
+            sw.Close();
+            sb.Close();
         }
-        static void Loading(string operacija, int brzina)
+        else
         {
-            Console.Write("{0}...", operacija);
-            StringBuilder sb = new StringBuilder("..", 3);
+            sw = new StreamWriter("desifrovana_" + operacija.datoteka);
+            while (sb.EndOfStream)
+            {
+                char[] temp = sb.ReadLine().ToCharArray();
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    temp[i] -= operacija.kljuc[i % operacija.kljuc.Length];
+                }
+                sw.WriteLine(temp);
+            }
+            sw.Close();
+            sb.Close();
+        }
+        return true;//ako sve prodje vraca tacno
+    }
+    static void IspisRadaPrograma(string greska)
+    {
+        Console.WriteLine("program radi tako sto:");
+        Console.WriteLine("bla bla bla");
+        Console.WriteLine(greska);
+    }
+    static void Loading(string operacija, int brzina)
+    {
+        Console.Write("{0}...", operacija);
+        StringBuilder sb = new StringBuilder("..", 3);
+        Console.SetCursorPosition(0, Console.CursorTop + 1);
+        for (int i = 0; i < 2; i++)
+        {
+            Thread.Sleep(brzina);
+            Console.SetCursorPosition(operacija.Length, Console.CursorTop - 1);
+            Console.Write("{0,-3}", sb);
             Console.SetCursorPosition(0, Console.CursorTop + 1);
-            for (int i = 0; i < 2; i++)
-            {
-                Thread.Sleep(brzina);
-                Console.SetCursorPosition(operacija.Length, Console.CursorTop - 1);
-                Console.Write("{0,-3}", sb);
-                Console.SetCursorPosition(0, Console.CursorTop + 1);
-                if (sb.ToString() != ".") sb = sb.Remove(sb.Length - 1, sb.Length - 1);
-                else sb = new StringBuilder("", 3);
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                Thread.Sleep(brzina);
-                Console.SetCursorPosition(operacija.Length, Console.CursorTop - 1);
-                Console.Write("{0,-3}", sb);
-                Console.SetCursorPosition(0, Console.CursorTop + 1);
-                sb.Append('.');
-            }
-            Console.WriteLine("uspesno izvrseno {0} ",operacija);
+            if (sb.ToString() != ".") sb = sb.Remove(sb.Length - 1, sb.Length - 1);
+            else sb = new StringBuilder("", 3);
         }
-        static void Main(string[] args)
+        for (int i = 0; i < 4; i++)
         {
-            Operacija[] operacije = new Operacija[2];//umesto 2 treba da stoji args.Length/4 ali jos nemam argumente, i ovo treba tek kasnije da se radi kada se argumenti provere
-          
-            //pre ovoga treba da se urade sve operacije nad fajlovima i provere
-          
-            for (int i = 0; i < operacije.Length; i++)
-            {
-                if (operacije[i].sifrovanje) Loading("Sifrovanje datoteke " + operacije[i].datoteka, 250);
-                else Loading("Desifrovanje datoteke " + operacije[i].datoteka, 250);
-            }
-            
+            Thread.Sleep(brzina);
+            Console.SetCursorPosition(operacija.Length, Console.CursorTop - 1);
+            Console.Write("{0,-3}", sb);
+            Console.SetCursorPosition(0, Console.CursorTop + 1);
+            sb.Append('.');
         }
+        Console.WriteLine("uspesno izvrseno {0} ", operacija);
+    }
+    static void Main(string[] args)
+    {
+        Operacija[] operacije = new Operacija[2];//umesto 2 treba da stoji args.Length/4 ali jos nemam argumente, i ovo treba tek kasnije da se radi kada se argumenti provere
+
+        //pre ovoga treba da se urade sve operacije nad fajlovima i provere
+
+        for (int i = 0; i < operacije.Length; i++)
+        {
+            if (operacije[i].sifrovanje) Loading("Sifrovanje datoteke " + operacije[i].datoteka, 250);
+            else Loading("Desifrovanje datoteke " + operacije[i].datoteka, 250);
+        }
+
+    }
 }
